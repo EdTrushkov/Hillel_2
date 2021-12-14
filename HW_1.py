3f0e95b9af839c02d7fb3308781e66444ad56e35
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-12-14 17:23:27 +0200

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -1,18 +1,23 @@
-"""Сделать декоратор cache, который бы кэшировал данные не больше чем на N секунд.
- N должно быть параметром декоратора"""
-
+import functools
 import time
 
 
 def cache(N):
     memory = {}
 
+    @functools.wraps(N)
     def check_time(*args):
-        if args not in memory:
-            memory[args] = time.time()
-        elif args in memory:
-            if memory[args] - time.time() < N:
-                print(memory[args])
+        for i in args:
+            if i not in memory:
+                memory[i] = time.time()
+            elif i in memory:
+                if time.time() - memory[i] < i:
+                    print(i, memory[i])
+                else:
+                    del memory[i]
     return check_time
 
 
+@cache
+def N(times):
+    return times
