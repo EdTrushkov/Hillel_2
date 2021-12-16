ffe9ff95970610f7be53459e6f785fc1cffff491
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-12-16 15:21:31 +0200

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -6,17 +6,18 @@ def cache(times):
 
     def decorator(f):
         cache_obj = {}
+        cache_time = {}
 
         @functools.wraps(f)
         def wrapper(*args):
-            if f not in cache_obj:
-                cache_obj[f] = time.time()
+            if args not in cache_obj:
+                cache_obj[args] = f(*args)
+                cache_time[args] = time.time()
                 print(f"Data {f}, add in cache")
-            else:
-                if time.time() - cache_obj[f] > times:
-                    del cache_obj[f]
-                    print(f"Data {f}, left time range")
-            return f(*args)
+            elif time.time() - cache_time[args] > times:
+                cache_obj[args] = f(*args)
+                cache_time[args] = time.time()
+            return cache_obj[args]
 
         return wrapper
     return decorator
